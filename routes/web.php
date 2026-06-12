e8f6d767806e85c796cbee3877ee720c5d8ee083
Nycolas-Lacerda <nycolaslp751@gmail.com>
2026-06-11 21:49:39 -0300

    fix: corrige rota /

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Models\Subject;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
